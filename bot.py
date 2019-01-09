358522bfbe0b8e24ace1ae9c595d54dccda204f3
dess00 <43251548+dess00@users.noreply.github.com>
2019-01-09 15:55:28 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,18 +40,4 @@ def default_test(message):
 def handle_stop(message):
     bot.send_message(message.chat.id, config.error)
     
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://miragn.herokuapp.com") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+bot.polling(none_stop = True, interval=0 )  
